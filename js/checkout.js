4753c0d872640d8ace7100205e2f4c4e4ddfd3be
vSriiman <68470931+A1mpk@users.noreply.github.com>
2026-05-13 14:48:05 -0400

    Checkout/ConfirmationS

---
+++ js/checkout.js
@@ -0,0 +1,58 @@
+document.getElementById("checkoutForm").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let phone = document.getElementById("phone");
+    let address = document.getElementById("address");
+    let delivery = document.getElementById("delivery");
+    let error = document.getElementById("error");
+
+    name.style.border = "";
+    email.style.border = "";
+    phone.style.border = "";
+    address.style.border = "";
+    delivery.style.border = "";
+
+    if(name.value == ""){
+        name.style.border = "2px solid red";
+    }
+
+    if(email.value == ""){
+        email.style.border = "2px solid red";
+    }
+
+    if(phone.value == "") {
+        phone.style.border = "2px solid red";
+    }
+
+    if(address.value == ""){
+        address.style.border = "2px solid red";
+    }
+
+    if(delivery.value == ""){
+        delivery.style.border = "2px solid red";
+    }
+
+    if(name.value == "" || email.value == "" || phone.value == "" || address.value == "" || delivery.value == ""){
+
+        error.innerHTML = "Please fill in all fields.";
+
+    } else{
+
+        error.innerHTML = "";
+
+        alert("Order Submitted!");
+
+        name.value = "";
+        email.value = "";
+        phone.value = "";
+        address.value = "";
+        delivery.value = "";
+
+        document.location = "checkout-confirmation.html";
+
+    }
+
+});
