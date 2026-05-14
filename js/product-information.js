61ffa3234f63cee0c34aee759fec4f203ae43d80
NeoZid <zidereckn06@gmail.com>
2026-05-13 20:08:23 -0400

    products.html and product-detail is now connected, customer reviews show in the bottom too, fixed some messy css styles

---
+++ js/product-information.js
@@ -0,0 +1,43 @@
+
+fetch('./data/products.json')
+    .then(response => response.json())
+    .then(products => {
+        const params = new URLSearchParams(window.location.search); // this extracts the url from the query we did from product-list
+        const id = Number(params.get('id')); // we put the extracted id into const id -- > number method parses string into int
+        const product = products.find(p => p.id == id); // matches productid from json to extracted product
+        
+        document.getElementById('product-image').src = product.image;
+        document.getElementById('product-image').alt = product.name;
+        document.getElementById('product-name').textContent = product.name;
+        document.getElementById('product-price').textContent = `Product Price: $${product.price}`;
+        document.getElementById('product-sku').textContent = `Product SKU: ${product.sku}`;
+        document.getElementById('product-availability').textContent = `Availability: ${product.availability}`;
+        document.getElementById('product-description').textContent = `Description: ${product.description}`;
+        document.getElementById('product-specs').innerHTML = `
+            <h1>Product Specs</h1>
+            <p>CPU: ${product.specs.cpu}</p>
+            <p>GPU: ${product.specs.gpu}</p>
+            <p>RAM: ${product.specs.ram}</p>
+            <p>Storage: ${product.specs.storage}</p>
+            <p>OS: ${product.specs.os}</p>
+        `;
+    });
+
+fetch('./data/reviews.json')
+    .then(response => response.json())
+    .then(reviews => {
+        const params = new URLSearchParams(window.location.search);
+        const id = Number(params.get('id'));
+        const productReviews = reviews.filter(r => r.productId == id);
+        productReviews.forEach(review => {
+            document.getElementById('reviews-list').innerHTML += `
+                <div class="review-card">
+                    <h3>${review.author} — ${review.rating}/5</h3>
+                    <p><strong>${review.title}</strong></p>
+                    <p>${review.body}</p>
+                    <p>${review.date}</p>
+            </div>
+        `;
+        });
+        
+    });
