79fcf21bcc3331d1f20cca559c508ace6bd91c55
vSriiman <68470931+A1mpk@users.noreply.github.com>
2026-05-14 16:27:33 -0400

    Check

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,26 @@
+let cartSummary = document.getElementById("cartSummary");
 
+let total = 0;
+
+let cookies = document.cookie.split("; ");
+
+let cartCookie = cookies.find(c => c.startsWith("cart="));
+
+if(cartCookie){
+
+    let cart = JSON.parse(cartCookie.split("=")[1]);
+
+    for(let i = 0; i < cart.length; i++){
+
+        cartSummary.innerHTML += "<p>" + cart[i].name + " - $" + cart[i].price + "</p>";
+
+        total += cart[i].price;
+
+    }
+
+    cartSummary.innerHTML += "<h3>Total: $" + total.toFixed(2) + "</h3>";
+
+}
 document.getElementById("checkoutForm").addEventListener("submit", function(event){
 
     event.preventDefault();
