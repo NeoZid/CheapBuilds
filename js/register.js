ec65eb40d0959a01d9ac9c68e9f3fbd51e2d761f
Yves <yves.picicur@gmail>
2026-05-14 20:15:27 -0400

    Created register page

---
+++ js/register.js
@@ -0,0 +1,24 @@
+const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+document.getElementById('register-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const password1 = document.getElementById('password1').value.trim();
+    const password2 = document.getElementById('password2').value.trim();
+
+    if (!email.match(emailPattern)) {
+        alert('Invalid Email Format');
+        return;
+    } 
+
+    if(password1 != password2) {
+        alert('Password must be the same');
+        return;
+    } else {
+        document.getElementById('email').value = "";
+        document.getElementById('password1').value = "";
+        document.getElementById('password2').value = "";
+    }
+
+});
